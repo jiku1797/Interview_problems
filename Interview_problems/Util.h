11196a957e9fe240705c9d2939d5158c40565b93
jiku1797 <jiku1797@gmail.com>
2022-01-30 14:41:48 +0100

    Add files via upload

---
+++ Interview_problems/Util.h
@@ -0,0 +1,15 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+struct pair_hash
+{
+    template <class T1, class T2>
+    std::size_t operator () (std::pair<T1, T2> const &pair) const
+    {
+        std::size_t h1 = std::hash<T1>()(pair.first);
+        std::size_t h2 = std::hash<T2>()(pair.second);
+
+        return h1 ^ h2;
+    }
+};
+#endif // UTIL_H
